fb0ea53793952167c86de5fc24bdd2dc780e2efe
LAPTOP-8BMLLQCD\HUY <tranthanhhuy2207@gmail.com>
2021-12-08 21:51:36 +0700

    optimize project

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -5,8 +5,8 @@ import {
   HttpEvent,
   HttpInterceptor,
 } from '@angular/common/http';
-import { Observable, of, throwError } from 'rxjs';
-import { catchError, concatMap, delay, retryWhen } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { AuthService } from '../services';
 import { Router } from '@angular/router';
 
@@ -21,13 +21,11 @@ export class ErrorInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       catchError((err) => {
-        if (err.status === 404) {
-          this.router.navigate(['/404']);
-        }
-         else if (err.status === 401) {
+        if (err.status === 401) {
           this.authService.logout();
         }
-
+        
+        this.router.navigate(['/404']);
         const error = err.error.message || err.statusText;
         return throwError(error);
       })
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -8,9 +8,7 @@ import { MatCommonModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
@@ -22,10 +20,8 @@ import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSliderModule } from '@angular/material/slider';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatSortModule } from '@angular/material/sort';
-import { MatStepperModule } from '@angular/material/stepper';
 import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -43,9 +39,7 @@ const materialModules = [
   MatDatepickerModule,
   MatDialogModule,
   MatDividerModule,
-  MatExpansionModule,
   MatFormFieldModule,
-  MatGridListModule,
   MatIconModule,
   MatInputModule,
   MatListModule,
@@ -57,10 +51,8 @@ const materialModules = [
   MatSelectModule,
   MatSidenavModule,
   MatSlideToggleModule,
-  MatSliderModule,
   MatSnackBarModule,
   MatSortModule,
-  MatStepperModule,
   MatTableModule,
   MatTabsModule,
   MatTooltipModule,
