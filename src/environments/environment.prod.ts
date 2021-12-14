f5f84cf4e320e7fffcd31f1a38901ce666a5962b
LAPTOP-8BMLLQCD\HUY <tranthanhhuy2207@gmail.com>
2021-12-14 11:35:17 +0700

    Fix env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://cow-management-service.herokuapp.com/api',
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'https://cow-management-service.herokuapp.com/api',
 };
